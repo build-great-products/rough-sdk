b8bb862f30f83425cbe4ca18c0f5a3ba28540c05
George Czabania <1140167+stayradiated@users.noreply.github.com>
2025-12-06 09:10:58 +0800

    feat(api): fetch and save OpenAPI schema for client generation (#12)

--- openapi-ts.ts
+++ openapi-ts.ts
@@ -1,8 +1,25 @@
+import { writeFile } from 'node:fs/promises'
 import { createClient } from '@hey-api/openapi-ts'
 
+// production
+const schemaUrl = 'https://in.rough.app/api/v1/openapi.json'
+
+// development
+// const schemaUrl = 'http://localhost:5173/api/v1/openapi.json'
+
+console.info(`Fetching schema from ${schemaUrl}`)
+
+const schemaResponse = await fetch(schemaUrl)
+const schemaJSON = await schemaResponse.json()
+
+console.info(`Writing schema to openapi.json`)
+
+await writeFile('openapi.json', JSON.stringify(schemaJSON, null, 2))
+
+console.info(`Generating client from openapi.json`)
+
 createClient({
-  input: 'https://in.rough.app/api/v1/openapi.json',
-  // input: 'http://localhost:5173/api/v1/openapi.json',
+  input: 'openapi.json',
   output: 'src/api',
   plugins: ['@hey-api/client-fetch'],
 })
